15e20b4cf276dfa11046cd86a651b00d5869960c
fadzniaidil <aidilfadzni@gmail.com>
2021-09-30 16:43:07 +0800

    cap

--- pkptdev/application/views/Master/Admin/Content/Interviewer/admin_content.php
+++ pkptdev/application/views/Master/Admin/Content/Interviewer/admin_content.php
@@ -1,4 +1,4 @@
 <div class="content-wrapper">
   <?php $this->load->view('Master/Admin/content_header'); ?>
-  <?php $this->load->view('Master/Admin/Content/interviewer/interviewer_body'); ?>
+  <?php $this->load->view('Master/Admin/Content/Interviewer/interviewer_body'); ?>
 </div>
--- pkptdev/application/views/Master/Admin/Content/Orientation/admin_content.php
+++ pkptdev/application/views/Master/Admin/Content/Orientation/admin_content.php
@@ -1,4 +1,4 @@
 <div class="content-wrapper">
   <?php $this->load->view('Master/Admin/content_header'); ?>
-  <?php $this->load->view('Master/Admin/Content/orientation/orientation_content'); ?>
+  <?php $this->load->view('Master/Admin/Content/Orientation/orientation_content'); ?>
 </div>

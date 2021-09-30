84ea41b61835d74b798c16795561e2624cc6dec3
fadzniaidil <aidilfadzni@gmail.com>
2021-09-30 23:31:34 +0800

    add meta

--- pkptdev/application/views/Master/Admin/admin_header.php
+++ pkptdev/application/views/Master/Admin/admin_header.php
@@ -1,3 +1,5 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
 <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/fontawesome-free/css/all.min.css">
 <link rel="stylesheet" href="<?php echo base_url(); ?>assets/dist/css/adminlte.min.css">
--- pkptdev/application/views/Master/Login/login_header.php
+++ pkptdev/application/views/Master/Login/login_header.php
@@ -1,3 +1,4 @@
+<meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="<?php echo base_url()?>assets/plugins/fontawesome-free/css/all.min.css">
 <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
